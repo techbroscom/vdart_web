29f942cb969b12b1854c5b525f531fb7be064560
manisankarsms <manisankarsms@gmail.com>
2024-03-18 09:51:01 +0530

    added transactions

--- excelUtils.js
+++
@@ -1,8 +0,0 @@
-// excelUtils.js
-function arrayToExcel(array, filename) {
-    const ws = XLSX.utils.aoa_to_sheet(array);
-    const wb = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
-    XLSX.writeFile(wb, filename + '.xlsx');
-  }
-  

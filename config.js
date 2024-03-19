b6d7fd360b2e70fc0a0d294bebbad960dfc96079
manisankarsms <manisankarsms@gmail.com>
2024-03-20 01:25:30 +0530

    common config

---
+++ config.js
@@ -0,0 +1,18 @@
+// firebase-config.js
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCvcxuv1-MWBtHMIUkeqGH_gfyBWaNN7K0",
+    authDomain: "vdarttest.firebaseapp.com",
+    projectId: "vdarttest",
+    storageBucket: "vdarttest.appspot.com",
+    messagingSenderId: "157342186908",
+    databaseURL: "https://vdarttest-default-rtdb.asia-southeast1.firebasedatabase.app",
+    appId: "1:157342186908:web:ea6c53598d3e9d1cab9c59",
+    measurementId: "G-NVXLVD13D1"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
